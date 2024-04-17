76e91fcdab1a5c5259d3860a792c88b301c3f3bb
AdriVelasco2 <isadri35@hotmail.com>
2024-04-17 14:24:52 +0200

    Pequeñas correcciones de rendimiento

---
+++ .netlify/functions-internal/ssr/chunks/index_DG8SOR3F.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_Cc7X6zpO.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_CAOTsCEY.mjs';
+import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./chunks/generic_BxnTN5UD.mjs');
+const _page1 = () => import('./chunks/index_DG8SOR3F.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "edeb6f6f-28d8-4b1e-820c-54f9a7bb86c1"
+};
+const _exports = serverEntrypointModule.createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
