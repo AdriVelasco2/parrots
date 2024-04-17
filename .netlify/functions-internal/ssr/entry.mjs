55d143a449709f693c64338d327b313c170fb2e8
AdriVelasco2 <isadri35@hotmail.com>
2024-04-17 12:09:26 +0200

    Pequeñas correcciones

---
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_iVRueKeH.mjs';
+import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./chunks/generic_BxnTN5UD.mjs');
+const _page1 = () => import('./chunks/index_DP4_rtVN.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "4ac19a79-af6d-4174-a2de-df24b2f83fec"
+};
+const _exports = serverEntrypointModule.createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
