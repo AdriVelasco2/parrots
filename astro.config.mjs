da6f19f4c7d6b710cd64e6334c652d92437b2fd3
AdriVelasco2 <isadri35@hotmail.com>
2025-06-06 14:37:21 +0200

    Nueva navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ export default defineConfig({
   output: "server",
   adapter: netlify(),
   env: {
-    AIRTABLE_BASE_ID: import.meta.env.AIRTABLE_TOKEN,
-    AIRTABLE_TOKEN: import.meta.env.AIRTABLE_TOKEN,
-    AIRTABLE_TABLE_ID: import.meta.env.AIRTABLE_TABLE_ID
+    SUPABASE_URL: process.env.SUPABASE_URL || "https://aggaqlbtadudarkstveu.supabase.co",
+    SUPABASE_KEY: process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnZ2FxbGJ0YWR1ZGFya3N0dmV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NTM1ODYsImV4cCI6MjA2NDEyOTU4Nn0.BwNiJYIMUggCNv8NLxP2gXkursnk1xsJF3c7n-CXF88",
+
   },
 });
